85e24389db75e1154999df70b35414c4716b6788
leabiv <leabiv@gmail.com>
2022-08-30 15:10:48 -0500

    Ejercicio de promesaJS

---
+++ app.js
@@ -0,0 +1,60 @@
+const myPromesas = new Promise((resuelto, rechazado) => {
+    let coneccion = true;
+    if (coneccion) {
+        resuelto('Conexion Exitosa')
+    } else {
+        rechazado('Conexion Fallida')
+    }
+});
+/*
+myPromesas.then((message) => {
+    console.log(message)
+}).catch((message) => {
+    console.log(message)
+});
+*/
+
+// hacer una función que tome un solo parámetro
+// y devuelve una nueva promesa. usando setTimeout,
+// después de 500 milisegundos, la promesa será
+// resolver o rechazar. si la entrada es una cadena,
+// la promesa se resuelve con esa misma cadena
+// en mayúsculas. si la entrada es cualquier cosa menos una cadena
+// rechaza con esa misma entrada
+//
+// llamar a la función delayUpperCase
+
+/**
+ * @param {string} resolve - cumplida 
+ * @param {string} reject - rechazada
+ * @return {string} 
+ */
+const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (delayedUpperCase(parametro)) {
+            resolve(parametro.toUpperCase())
+        } else {
+            reject(parametro);
+        }
+    }, 500);
+});
+
+/**
+ * function que recibe la cadena
+ * @param {string} parametro - cadena
+ * @return {boolean} retorna true o false
+ */
+let parametro = 'hola mundo';
+function delayedUpperCase(parametro) {
+    if (typeof parametro === 'string' || parametro instanceof String) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+myPromise.then((message) => {
+    console.log(message)
+}).catch((message) => {
+    console.log(message)
+});
